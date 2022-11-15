5dda0301537b924b52e424603efd59aab9c05f23
sarah benzi <sarahlover822@gmail.com>
2022-11-14 22:01:13 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,51 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log:['query'],
+})
+
+
+async function start() {
+    const fastify = Fastify({
+    //serve pra monitorar a aplicacao 
+       logger:true,
+    })
+
+    //  é pra dizer quais aplicacoes vao poder consumir o nosso back
+    await fastify.register(cors,{
+        origin:true
+        })
+
+        
+
+    // rota de contagem de bolao(é o que vai dps de http://localhost/)
+    fastify.get('/pools/count' ,async () =>{
+
+
+    //    const pools= await prisma.pool.findMany({
+    //         where:{
+    //             title:{
+    //                equals: 'quem que dinheiro'
+    //             }
+    //         }
+    //     })
+
+        // return{}
+
+  
+        const qtdpools = await prisma.pool.count({
+            
+        })
+
+
+        return { qtdpools}
+    }
+
+    )
+// o host é pra funcionar no mobile
+    await fastify.listen({ port:3333, host:'0.0.0.0'})
+}
+
+start()
