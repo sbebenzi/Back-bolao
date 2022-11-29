cb67c9b2a03fa52a161df5b279e47212fc05f510
sarah benzi <sarahlover822@gmail.com>
2022-11-28 21:11:59 -0300

    criado seed de bolao

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,20 @@ import {PrismaClient} from "@prisma/client"
 const prisma = new PrismaClient()// conexao com banco
 
 async function main(){
-    const user = prisma.user.create({
+    const user = await prisma.user.create({
         data:{
             name:'maria antonieta',
             email:'mariaAntonieta@gmail.com',
             avatarUrl:'https://static.wikia.nocookie.net/naruto/images/3/33/Naruto_Uzumaki_%28Parte_I_-_HD%29.png/revision/latest?cb=20160316113315&path-prefix=pt-br'
             
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data:{
+            title: 'exemple pool',
+            code:'boll0',
+            ownerId:   user.id,
         }
     })
 }
