c5020990af3ac847de3f3c9132978d1c85e6e8ed
sarah benzi <sarahlover822@gmail.com>
2022-11-30 16:41:07 -0300

    nome da rota alterado para ficar padrao

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ async function start() {
         
 
     // rota de contagem de bolao(é o que vai dps de http://localhost/)
-    fastify.get('/pools/count' ,async () =>{  
+    fastify.get('/count/pools' ,async () =>{  
         const qtdpools = await prisma.pool.count()
         return { qtdpools}
     })
@@ -61,7 +61,7 @@ async function start() {
     })
 
     // rota de contagem de palpites
-    fastify.get('/count/guess' ,async () =>{  
+    fastify.get('/count/guesses' ,async () =>{  
         const qtdGuess = await prisma.guess.count()
         return { qtdGuess}
     })
